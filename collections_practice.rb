6fa0d0b816360e1ec41acd221d12669fbd6a967f
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-28 07:52:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,3 +22,11 @@ end
 def reverse_array(integers)
   new_array = integers.reverse new_array
 end
+
+def kesha_maker(array)
+  kesha = []
+    array.each do |word|
+      word_array = word.split ""
+      word_array[2] = "$"
+      kesha << word_array.join
+    end
